708bac08e73d1602fcf6607838e9d0c2f4c7654b
todd_pod <darren_liu1989@163.com>
2019-11-27 10:02:32 +0800

    Create config.js

---
+++ config.js
@@ -0,0 +1,12 @@
+impore base64
+import re
+import datetime,sys
+
+$host = "smtp.exmail.qq.com";
+$user = "xxxx@tencent.com"
+$password = "&@#nj1$!)kd"
+$secId = "AKIDX2ObCeKifWZVOBEDSp29azEQRGvSfPWR";
+$secKey = "adsfqas124351"
+
+
+
