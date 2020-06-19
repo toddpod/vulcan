c93c3bea6210ac47d1b88bc47f540b4845e1e785
todd_pod <darren_liu1989@163.com>
2020-06-19 17:25:48 +0800

    Update config.js

--- config.js
+++ config.js
@@ -3,10 +3,10 @@ import re
 import datetime,sys
 
 $host = "smtp.exmail.qq.com";
-$user = "xxxx@tencent.com"
+$user = "xxxx@fivecent.com"
 $password = "&@#nj1$!)kd"
 $secId = "AKIDX2ObCeKifWZVOBEDSp29azEQRGvSfPWS";
-$secKey = "adsfqas124351"
+$secKey = "seckeyisaseckeynots124351"
 
 
 
